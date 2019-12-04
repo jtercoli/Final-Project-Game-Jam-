18e16c88c44fded3c3686d436dd943a83171532b
Jadan Ercoli <jtercoli@iu.edu>
2019-12-03 23:55:19 -0500

    Updates HUD,character, and RealWorld.tscn

--- Godot/Assets/character.gd
+++ Godot/Assets/character.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed=10
+var speed=20
 func _ready():
 	set_process_input(true)
 	#connect("score",WorldNODE,"increase_score")
@@ -8,12 +8,19 @@ func _ready():
 func _input(event):
 	if Input.is_action_pressed("up"):
 		position.y -= speed
+		$AnimatedSprite.play("Flying")
+	else:
+		$AnimatedSprite.stop()
 	if Input.is_action_pressed("down"):
 		position.y += speed
+		$AnimatedSprite.play("Flying")
 	if Input.is_action_pressed("left"):
 		position.x -= speed
+		$AnimatedSprite.play("Flying")
 	if Input.is_action_pressed("right"):
 		position.x += speed
+		$AnimatedSprite.play("Flying")
+
 #func _physics_process(delta):
 	#var bodies = get_colliding_bodies()
 	#for body in bodies:
